2f5171dfeda138d7863d17888f1f0979f80881a0
offxnder <88309766+offxnder@users.noreply.github.com>
2022-12-22 12:32:35 +1100

    Update console.lua

--- workspace/console.lua
+++ workspace/console.lua
@@ -1,1 +1,29 @@
+-- Console Script - Offend Cmd
+consolecreate()
+consolesettitle("Offend Cmd - Console")
 
+local admin_script = loadstring(readfile("offend_cmd/admin.lua"))
+
+consoleprint([[
+
+     ██████╗ ███████╗███████╗███████╗███╗   ██╗██████╗      ██████╗███╗   ███╗██████╗ 
+    ██╔═══██╗██╔════╝██╔════╝██╔════╝████╗  ██║██╔══██╗    ██╔════╝████╗ ████║██╔══██╗
+    ██║   ██║█████╗  █████╗  █████╗  ██╔██╗ ██║██║  ██║    ██║     ██╔████╔██║██║  ██║
+    ██║   ██║██╔══╝  ██╔══╝  ██╔══╝  ██║╚██╗██║██║  ██║    ██║     ██║╚██╔╝██║██║  ██║
+    ╚██████╔╝██║     ██║     ███████╗██║ ╚████║██████╔╝    ╚██████╗██║ ╚═╝ ██║██████╔╝
+     ╚═════╝ ╚═╝     ╚═╝     ╚══════╝╚═╝  ╚═══╝╚═════╝      ╚═════╝╚═╝     ╚═╝╚═════╝ 
+
+]], "red")
+
+consoleprint(" - For a list of commands, Type 'cmds'\n\n", "green")
+
+function cmd()
+    consoleprint(" > ", "white")
+    local requested_cmd = consoleinput()
+
+    admin_script:fire(requested_cmd)
+
+    cmd()
+end
+
+cmd()
