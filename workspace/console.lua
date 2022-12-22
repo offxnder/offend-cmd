7a9ced3b54328f9b96a9a41c91c759c28d079cb6
offxnder <88309766+offxnder@users.noreply.github.com>
2022-12-22 14:18:10 +1100

    Update console.lua

--- workspace/console.lua
+++ workspace/console.lua
@@ -21,7 +21,17 @@ function cmd()
     consoleprint(" > ", "white")
     local requested_cmd = consoleinput()
 
-    admin_script:fire(requested_cmd)
+    local firecmd = admin_script:fire(requested_cmd)
+
+    pcall(function()
+        if tostring(firecmd) then
+            print(firecmd.."\n")
+        else
+            for i, sdf in pairs(firecmd) do
+                print(sdf.."\n")
+            end
+        end
+    end)
 
     cmd()
 end
